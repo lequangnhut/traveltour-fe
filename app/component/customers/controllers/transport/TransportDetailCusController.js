0462366c6b60cd7c63d0e3a47771348824fbed02
annt <anntpc04830@fpt.edu.vn>
2024-03-11 22:31:55 +0700

    Giao diện xe

--- app/component/customers/controllers/transport/TransportDetailCusController.js
+++ app/component/customers/controllers/transport/TransportDetailCusController.js
@@ -1,3 +1,13 @@
 travel_app.controller('TransportDetailCusController', function ($scope) {
+    $scope.showTransportInfoNav = false;
 
+    $scope.toggleTransportInfoNav = function () {
+        $scope.showTransportInfoNav = !$scope.showTransportInfoNav;
+    }
+
+    $scope.currentTab = 'pending';
+
+    $scope.changeTab = function(tabName) {
+        $scope.currentTab = tabName;
+    };
 });
