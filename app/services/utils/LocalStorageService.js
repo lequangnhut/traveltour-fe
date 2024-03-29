4f482d7549a7707582de41b0164147ef11e5f151
NguyenDyKhang-k3 <138186065+NguyenDyKhang-k3@users.noreply.github.com>
2024-03-29 20:31:40 +0700

    cạp nhật phương thức mã hóa

--- app/services/utils/LocalStorageService.js
+++ app/services/utils/LocalStorageService.js
@@ -45,4 +45,35 @@ travel_app.service('LocalStorageService', function () {
         }
     };
 
+    this.encryptLocalData = (data, localKey, customKey) => {
+        const combinedKey = customKey + defaultKey;
+
+        if (!data) {
+            console.error('dữ liệu không xác định hoặc null');
+            return null;
+        }
+
+        let encryptData = CryptoJS.AES.encrypt(JSON.stringify(data), combinedKey).toString();
+        return localStorage.setItem(localKey, JSON.stringify(encryptData));
+    };
+
+    this.decryptLocalData = (localKey, customKey) => {
+        const combinedKey = customKey + defaultKey;
+
+        try {
+            const value = localStorage.getItem(localKey);
+            let encryptedData = value ? JSON.parse(value) : null;
+
+            if (!encryptedData) return null;
+
+            let bytes = CryptoJS.AES.decrypt(encryptedData, combinedKey);
+            let decryptedText = bytes.toString(CryptoJS.enc.Utf8);
+
+            return decryptedText ? JSON.parse(decryptedText) : null;
+        } catch (e) {
+            console.error('lỗi giải mã dữ liệu:', e);
+            return null;
+        }
+    };
+
 });
